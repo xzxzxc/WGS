1af3af944473be24dde46fce5c62f25986ac2455
Vladeeny <Vladlib@i.ua>
2016-04-17 14:55:15 +0300

    first commit. view changed

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# Create your views here.
+from polls.forms import VisitorMessageForm
+from polls.models import VisitorMessage
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+
+# def my(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    if request.method == "POST":
+        form = VisitorMessageForm(request.POST)
+        message = form.cleaned_data['message']
+        obj = VisitorMessage(message=message)
+        obj.save()
+    return render(request, "home.html")
